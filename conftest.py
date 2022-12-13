82cc2d3e19b9189b15e4a376f194d0e9c2e97952
MarySaf1 <saf.mary.y@gmail.com>
2022-12-13 12:33:49 +0300

    - тесты

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome')
+    parser.addoption('--language', action='store', default='en')
+
+
+@pytest.fixture(scope="function")
+def browser(user_language=None):
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
